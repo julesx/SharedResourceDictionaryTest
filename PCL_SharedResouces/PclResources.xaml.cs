b16d80a62571632ab39aafbf07ab89c140bce463
Julien Rosen <jlrosen416@gmail.com>
2017-04-03 14:55:57 -0400

    Add project files.

---
+++ PCL_SharedResouces/PclResources.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PCL_SharedResouces
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PclResources
+    {
+        public PclResources()
+        {
+
+        }
+    }
+}
